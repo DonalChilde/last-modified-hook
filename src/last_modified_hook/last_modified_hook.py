d288f338e60e15714561ca49f0380e9656cde337
Chad Lowe <pfmsoft.dev@gmail.com>
2022-12-03 12:40:35 -0700

    update code to use new find and replace snippet

--- src/last_modified_hook/last_modified_hook.py
+++ src/last_modified_hook/last_modified_hook.py
@@ -2,51 +2,82 @@ import argparse
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from string import Template
-from typing import Sequence, Tuple
+from typing import Sequence
 
-from last_modified_hook.find_and_replace import ModifiedLine, find_and_replace
+from last_modified_hook.find_and_replace import (
+    FileLineProtocol,
+    LineMatcherProtocol,
+    LineReplacerProtocol,
+    MatchedLine,
+    MatchedLineProtocol,
+    ModifiedLine,
+    find_and_replace,
+)
 
 
-class LastModifiedReplacer:
-    def __init__(
-        self, seconds: int = 60, line_limit: int = -1, line_template: str = ""
-    ) -> None:
+class LastModifiedMatcher(LineMatcherProtocol):
+    def __init__(self, line_limit: int = -1) -> None:
         self.line_limit = line_limit
+
+    def match(self, file_line: FileLineProtocol) -> MatchedLine:
+        if self.line_limit == -1 or self.line_limit >= file_line.line_no:
+            if file_line.line.startswith("# Last Modified:"):
+                values = file_line.line.split()
+                try:
+                    last_modified = datetime.fromisoformat(values[3])
+                    return MatchedLine(
+                        is_match=True,
+                        file_line=file_line,
+                        matched_values={"last_modified": last_modified},
+                    )
+                except (IndexError, ValueError):
+                    return MatchedLine(
+                        is_match=True,
+                        file_line=file_line,
+                        matched_values={"last_modified": None},
+                    )
+        return MatchedLine(
+            is_match=False,
+            file_line=file_line,
+            matched_values={},
+        )
+
+
+class LastModifiedReplacer(LineReplacerProtocol):
+    def __init__(self, seconds: int = 60, line_template: str = "") -> None:
+
         self.seconds = seconds
         if not line_template:
-            self.line_template = Template("# Last Modified: $_iso_date_         #")
+            self.line_template = Template("# Last Modified: $_iso_date_  #\n")
         else:
             self.line_template = Template(line_template)
 
-    def __call__(self, filename: str, line_no: int, line: str) -> ModifiedLine:
-        if self.line_limit == -1 or self.line_limit >= line_no:
-            if line.startswith("# Last Modified:"):
-                return self._modify_line(line)
-            else:
-                return ModifiedLine(False, line)
-        else:
-            return ModifiedLine(False, line)
+    def replace(self, matched_line: MatchedLineProtocol) -> ModifiedLine:
+        return self._modify_line(matched_line)
 
-    def _modify_line(self, line) -> ModifiedLine:
-        if self._check_within_seconds(line):
-            return ModifiedLine(False, line)
-        return ModifiedLine(True, self._generate_line())
+    def _modify_line(self, matched_line: MatchedLineProtocol) -> ModifiedLine:
+        if self._check_within_seconds(matched_line):
+            return ModifiedLine(
+                False, original_line=matched_line, new_line=matched_line.file_line.line
+            )
+        return ModifiedLine(
+            True, original_line=matched_line, new_line=self._generate_line()
+        )
 
-    def _check_within_seconds(self, line: str) -> bool:
-        values = line.split()
-        try:
-            last_modified = datetime.fromisoformat(values[2])
-            now = datetime.now(timezone.utc)
-            if now - last_modified <= timedelta(seconds=self.seconds):
-                return True
-        except:
-            pass
+    def _check_within_seconds(self, matched_line: MatchedLineProtocol) -> bool:
+        last_modified = matched_line.matched_values.get("last_modified", None)
+        if last_modified:
+            try:
+                now = datetime.now(timezone.utc)
+                if now - last_modified <= timedelta(seconds=self.seconds):
+                    return True
+            except ValueError:
+                pass
         return False
 
     def _generate_line(self) -> str:
         now = datetime.now(timezone.utc)
         new_line = self.line_template.safe_substitute(_iso_date_=now.isoformat())
-        print(new_line)
         return new_line
 
 
@@ -77,14 +108,17 @@ def cli(argv: Sequence[str] | None = None) -> int:
     )
     args = parser.parse_args(argv)
     replacer = LastModifiedReplacer()
+    matcher = LastModifiedMatcher()
     retv = 0
     for filename in args.filenames:
         try:
-            dirty = find_and_replace(file=Path(filename), replacer=replacer)
+            modified_lines = find_and_replace(
+                file_path=Path(filename), replacer=replacer, matcher=matcher
+            )
         except Exception as error:
             print(f"{error!r}")
             return 1
-        if dirty:
+        if modified_lines:
             print(f"last_modified_hook updated file: {filename}")
             retv = 1
     return retv
