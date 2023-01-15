c464ebe1f9c018503e584affafab91d03ecda8dd
Chad Lowe <pfmsoft.dev@gmail.com>
2023-01-15 10:37:58 -0700

    single source project metadata in pyproject.toml

--- src/last_modified_hook/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present Chad Lowe <pfmsoft.dev@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "v0.0.4"
--- src/last_modified_hook/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present Chad Lowe <pfmsoft.dev@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-import last_modified_hook.__about__
